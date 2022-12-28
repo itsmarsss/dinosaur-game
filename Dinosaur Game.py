44872e6043a42f1e099ffa4488443f9e3c7bebf5
itsmarsss <itsmarzzzzzz@gmail.com>
2022-12-28 00:37:35 -0500

    Import all necessary assets

--- Dinosaur Game.py
+++ Dinosaur Game.py
@@ -1,23 +1,41 @@
 from tkinter import *
-
-
-def dinoJump():
-    pass
-
-
-def dinoDuch():
-    pass
-
-
-def onKeyPress(event):
-    key = event.keysym
-    if key == 'Up':
-        dinoJump()
-    elif key == 'Down':
-        dinoDuch()
+from PIL import ImageTk, Image
+import os
 
 gameWindow = Tk()
+playerImage = Image.open((os.getcwd() + '/assets/allsprites.png'))
+playerImage = playerImage.resize((50, 50), Image.ANTIALIAS)
+playerImage = ImageTk.PhotoImage(playerImage)
+player = Label(gameWindow, image=playerImage)
+
+player.image = playerImage
+player.pack()
+
+
+async def dino_jump():
+    pass
+
+
+async def dino_duck():
+    pass
+
+
+async def generate_obstacle():
+    pass
+
+
+def on_key_press(event):
+    key = event.keysym
+    if key == 'Up':
+        dino_jump()
+    elif key == 'Down':
+        dino_duck()
+
+
 gameWindow.title('Dinosaur Game')
-gameWindow.bind('<KeyPress>', onKeyPress)
+gameWindow.bind('<KeyPress>', on_key_press)
+gameWindow.geometry('1200x500')
+gameWindow.minsize(1200, 500)
+gameWindow.maxsize(1200, 500)
 
 gameWindow.mainloop()
