df26678f1946c29a5ac42b53d719811601550cdd
itsmarsss <itsmarzzzzzz@gmail.com>
2022-12-28 15:39:03 -0500

    Make infinite scrolling floor

--- Dinosaur Game.py
+++ Dinosaur Game.py
@@ -2,18 +2,18 @@ import os
 
 import pygame
 
-WIDTH, HEIGHT = 1200, 300
+WIDTH, HEIGHT = 600, 200
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dinosaur Game")
 
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
 
 FPS = 60
 
 RUN1 = pygame.image.load(os.path.join('Assets', 'run-1.png'))
 RUN2 = pygame.image.load(os.path.join('Assets', 'run-2.png'))
 DUCK1 = pygame.image.load(os.path.join('Assets', 'duck-1.png'))
-DUCK1 = pygame.image.load(os.path.join('Assets', 'duck-1.png'))
+DUCK2 = pygame.image.load(os.path.join('Assets', 'duck-2.png'))
 DEAD = pygame.image.load(os.path.join('Assets', 'dead.png'))
 BIRD1 = pygame.image.load(os.path.join('Assets', 'bird-1.png'))
 BIRD2 = pygame.image.load(os.path.join('Assets', 'bird-2.png'))
@@ -27,15 +27,16 @@ CLOUD = pygame.image.load(os.path.join('Assets', 'cloud.png'))
 RESTART = pygame.image.load(os.path.join('Assets', 'restart.png'))
 GROUND = pygame.image.load(os.path.join('Assets', 'ground.png'))
 
-RUN1 = pygame.transform.scale(RUN1, (RUN1.get_width()/2, RUN1.get_height()/2))
-RUN2 = pygame.transform.scale(RUN2, (RUN2.get_width()/2, RUN2.get_height()/2))
-GROUND = pygame.transform.scale(GROUND, (GROUND.get_width()/2, GROUND.get_height()/2))
+RUN1 = pygame.transform.scale(RUN1, (RUN1.get_width() / 2, RUN1.get_height() / 2))
+RUN2 = pygame.transform.scale(RUN2, (RUN2.get_width() / 2, RUN2.get_height() / 2))
+GROUND = pygame.transform.scale(GROUND, (GROUND.get_width() / 2, GROUND.get_height() / 2))
 
 
-def draw_window(dino, dino_coords, floor, cacti):
+def draw_window(dino, dino_coords, floor, floor_coords, cacti):
     WIN.fill(WHITE)
     WIN.blit(dino, (dino_coords.x, dino_coords.y))
-    WIN.blit(GROUND, (0, 280))
+    WIN.blit(GROUND, (floor_coords.x, floor_coords.y))
+    WIN.blit(GROUND, (floor_coords.x + 1200, floor_coords.y))
     pygame.display.update()
 
 
@@ -43,7 +44,10 @@ def main():
     grav_acc = 1
     dino_vel = 0
     time = 0
-    dino_coords = pygame.Rect(20, 245, RUN1.get_width(), RUN1.get_height())
+    dino_count = 0
+    floor_count = 0
+    dino_coords = pygame.Rect(20, 145, RUN1.get_width(), RUN1.get_height())
+    floor_coords = pygame.Rect(0, 180, GROUND.get_width(), GROUND.get_height())
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -58,14 +62,26 @@ def main():
                 dino_vel = -12
 
         if dino_vel != 0:
-            dino_coords.y = 245+dino_vel*time+0.5*grav_acc*time*time
-            if dino_coords.y >= 245:
+            dino_coords.y = 145 + dino_vel * time + 0.5 * grav_acc * time * time
+            if dino_coords.y >= 145:
+                dino_coords.y = 145
                 dino_vel = 0
-                time=0
-            time=time+0.5
+                time = 0
+            time = time + 0.5
             if keys_pressed[pygame.K_DOWN]:
-                time=time+0.5
-        draw_window(RUN1, dino_coords, GROUND, SMALL1)
+                time = time + 0.5
+
+        dino_sprite = RUN1
+        dino_count = dino_count + 1
+        if dino_count <= FPS / 10:
+            dino_sprite = RUN2
+        dino_count = dino_count % (FPS / 5)
+
+        floor_coords.x = floor_coords.x - 5
+        if floor_coords.x == -1200:
+            floor_coords.x = 0
+
+        draw_window(dino_sprite, dino_coords, GROUND, floor_coords, SMALL1)
 
     pygame.quit()
 
